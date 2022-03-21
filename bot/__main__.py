154c01e194e993a853f43571567d00bbd80f4fa6
Nyobabot10 <101008992+Nyobabot10@users.noreply.github.com>
2022-03-21 11:26:38 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -46,7 +46,7 @@ def stats(update, context):
 def start(update, context):
     buttons = button_build.ButtonMaker()
     buttons.buildbutton("Index", "https://gawr-index.floral.workers.dev/1:")
-    buttons.buildbutton("Channel", "https://t.me/SlamMirrorUpdates")
+    buttons.buildbutton("Channel", "https://t.me/gawrmirrorchannel")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
